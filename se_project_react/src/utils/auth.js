1a999d872eb7a92903f32a770d30c4f32ff381be
Lydia Noh <lydia28abeck@gmail.com>
2026-03-20 04:30:10 -0400

    Deploy and hosting project in the cloud

--- se_project_react/src/utils/auth.js
+++ se_project_react/src/utils/auth.js
@@ -1,6 +1,9 @@
 import { checkResponse } from "./api";
 
-const BASE_URL = "http://localhost:3001";
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://api.wtwrtodayforyou.crabdance.com"
+    : "http://localhost:3001";
 
 export const register = ({ name, avatar, email, password }) =>
   fetch(`${BASE_URL}/signup`, {
